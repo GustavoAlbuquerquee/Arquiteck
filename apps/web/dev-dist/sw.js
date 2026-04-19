1a8ffe2c852ca999d716c30d33609297d544d058
Gustavo Albuquerque <gustavoalbuquerque807@gmail.com>
2026-04-19 20:53:41 -0300

    fix: corrige a revisão do arquivo index.html no service worker

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a731ab65'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h5umkj6dsgo"
+    "revision": "0.2otgm4rjfog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
